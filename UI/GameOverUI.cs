a1cf293c22b9f017edb64f6484ef5609ba739a91
Pedro <156934150+pedromussi1@users.noreply.github.com>
2024-03-21 14:56:57 -0400

    Add files via upload

---
+++ UI/GameOverUI.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI recipesDeliveredText;
+
+    [SerializeField] private Button restartButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        restartButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.GameScene);
+        });
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+
+        Time.timeScale = 1f;
+    }
+
+    private void Start()
+    {
+        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+
+        Hide();
+    }
+
+    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e)
+    {
+        if (KitchenGameManager.Instance.IsGameOver())
+        {
+            Show();
+            recipesDeliveredText.text = DeliveryManager.Instance.GetSuccessfulRecipesAmount().ToString();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+ 
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+
+    }
+
+}
